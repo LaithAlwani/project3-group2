d7b9e4e4f3b20a105b9e5679aeb09639e493f6a3
Laith Alwani <laithalwani@gmail.com>
2021-04-27 13:23:31 -0400

    added Navbar and change the flie structure to follow MVC pattern

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,35 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import PrivateRoute from "./components/routing/PrivateRoute";
-import Login from "./components/pages/Login";
-import Register from "./components/pages/Register";
-import ForgotPassword from "./components/pages/ForgotPassword";
-import ResetPassword from "./components/pages/ResetPassword";
-import Landing from "./components/pages/Landing";
-import Home from "./components/pages/Home"
+import PrivateRoute from "./routing/PrivateRoute";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import ForgotPassword from "./pages/ForgotPassword";
+import ResetPassword from "./pages/ResetPassword";
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <Router>
-      <div className="app">
+    <div className="app">
+      <Router>
         <Switch>
-          <PrivateRoute exact path="/portal" component={Landing} />
           <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/forgotpassword"component={ForgotPassword}/>
-          <Route exact path="/passwordreset/:resetToken"component={ResetPassword}/>
+          <>
+            <Navbar />
+            <PrivateRoute exact path="/portal" component={Landing} />
+            <Route path="/login" component={Login} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/forgotpassword" component={ForgotPassword} />
+            <Route
+              exact
+              path="/passwordreset/:resetToken"
+              component={ResetPassword}
+            />
+          </>
         </Switch>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 };
 
