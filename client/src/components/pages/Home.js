349b9a79608e0b04d3ff7a23d1e610be5d4b3eea
Laith Alwani <laithalwani@gmail.com>
2021-04-26 23:40:56 -0400

    added Team schema and update User schema

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
      return (
         <div>
-            <h1>Project 3 Group 2</h1>
+            <h1>Project 3 Group 2!!!!!</h1>
             <ul>
                 <li><Link to="/register"> Sign Up </Link></li>
                 <li><Link to="/login"> Login</Link></li>
