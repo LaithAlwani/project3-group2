4eb2dac41100ef9aa257a0c12dc34fd6037ca748
Magdi Tiea <magditiea@gmail.com>
2021-04-29 02:01:35 -0400

    responsive container added to signup/login

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
     <div>
-      <h1>My Team</h1>
+      <h1 id="title">My Team</h1>
 
+<div className="container">
       <Link to="/register" className="mr-4">
         Sign Up
       </Link>
+      <br></br>
       <Link to="/login"> Login</Link>
+    </div>
     </div>
   );
 };
