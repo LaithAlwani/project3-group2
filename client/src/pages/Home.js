5435ee4084f32c52221ea00e0e29462f40d5f2c6
Magdi Tiea <magditiea@gmail.com>
2021-04-28 20:57:13 -0400

    workbox and service worker registered for PWA

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
     <div>
       <h1>My Team</h1>
 
+<div id="wallpaper">
       <Link to="/register" className="mr-4">
         Sign Up
       </Link>
       <Link to="/login"> Login</Link>
+    </div>
     </div>
   );
 };
