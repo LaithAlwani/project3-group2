6b3a5cb06076ba12d0b3f7edfe31a8f1a3f72835
Magdi Tiea <magditiea@gmail.com>
2021-04-29 18:50:34 -0400

    added lower version of boostrap resolving the module not found issue

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,6 @@ import "../styles/Home.css";
 const Home = () => {
   return (
     <div>
-<<<<<<< HEAD
 
 <div id="titlebox">
       <h1 id="title">My Team</h1>
@@ -19,24 +18,6 @@ const Home = () => {
       </Link>
       <br></br>
       <Link to="/login"> Login</Link>
-=======
-      <div class="float">
-        {/* <h1 id="title">Management Systems</h1> */}
-
-        <div id="card">
-          <ul>
-            <div id="login">
-              <Link to="/login"> Login</Link>
-            </div>
-            OR
-            <br />
-            <div id="signup">
-              <Link to="/register"> Create a free account </Link>
-            </div>
-          </ul>
-        </div>
-      </div>
->>>>>>> main
     </div>
     </div>
   );
