86190eceabcdddacfe84c727c402a3838561b802
Magdi Tiea <magditiea@gmail.com>
2021-05-03 02:54:32 -0400

    background and footer added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,54 +1,80 @@
 import React from "react";
-import Jumbotron from "../components/jumbotron/jumbotron";
 import { Link } from "react-router-dom";
 import "../styles/Home.css";
-const logo = require("../styles/team.png");
-
-
-
+import "../styles/orange.png"
 const Home = () => {
+  
+
+
   return (
-    <div>
-      <div id="titlebox">
-      <h1 id="title">My</h1>
-      </div>
+      <div>
 
+        <div id="orange">
+{/* 
+        <div className="home"> */}
+          <Link to="/register" className="mr-4">
+            Sign Up
+          </Link>
 
-      <div className="bg_image"
-        style={{
-          backgroundImage: 'url('+logo+')',
-          backgroundSize: "cover",
-          height: "100vh",
-          // color: "#f5f5f5"
-        }}
-        ></div>
+          <br></br>
 
+         <Link to="/login" className="mr-4"> Login </Link>
+        {/* </div> */}
+        </div> 
+  
+   
+{/* footer */}
+       
 
-<div class="container" style={{position: "fixed"}}>
-    <div className="home"> 
-      <Link to="/register" className="mr-4">
-        Sign Up
-      </Link>
-      <br></br>
-      <Link to="/login"> Login</Link>
+<div class="footer-dark">
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-3 item">
+                
+            </div>
+            <div class="col-sm-6 col-md-3 item">
+                
+            </div>
+            <div class="col-md-6 item text">
+              
+            </div>
+            <i class="fa fa-github" aria-hidden="true"></i>
+
+        </div>
+        <p class="copyright">My Team © 2021</p>
     </div>
-   </div>
-
-
-   <div class="footer">
-
-
-  <div class="footer-content-right">
-  <a href="https://github.com/nashwalters"><img src="images/github.jpeg" class="icon-style" alt="Github icon"/></a>
-  <a href="https://github.com/LaithAlwani"><img src="images/github.jpeg" class="icon-style" alt="Github icon"/></a>
-  <a href="https://github.com/ttmgs"><img src="images/github.jpeg" class="icon-style" alt="Github icon"/></a>
+</footer>
 </div>
-</div>
-   </div>
-     );
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+   
+  )
 };
-
-
 
 export default Home;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* <div class="social"><a href="#"><i class="icon ion-social-github"></i></a><a href="#"><i class="icon ion-social-github"></i></a><a href="#"><i class="icon ion-social-github"></i></a></div> */}
