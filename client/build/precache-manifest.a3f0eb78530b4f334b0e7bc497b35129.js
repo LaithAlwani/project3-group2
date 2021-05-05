949f161b00c6f9f2df3f689e611e9ec68eadd3ff
Laith Alwani <laithalwani@gmail.com>
2021-05-04 23:35:05 -0400

    updated build folder

---
+++ client/build/precache-manifest.a3f0eb78530b4f334b0e7bc497b35129.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa93e06bf69f196c67dc",
+    "url": "/static/css/main.df89eaab.chunk.css"
+  },
+  {
+    "revision": "aa93e06bf69f196c67dc",
+    "url": "/static/js/main.d1e13eed.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c805ba533dc27ce41c9c",
+    "url": "/static/js/2.0eeb3764.chunk.js"
+  },
+  {
+    "revision": "1fb16dd05717b4825bf0bbe448865db8",
+    "url": "/static/media/flow.1fb16dd0.jpg"
+  },
+  {
+    "revision": "dd5d205d2e18ee8bde073ca51ead65da",
+    "url": "/static/media/image.dd5d205d.png"
+  },
+  {
+    "revision": "e5ec82fac597dd2183da4abeed2f1744",
+    "url": "/static/media/orange.e5ec82fa.png"
+  },
+  {
+    "revision": "15f164c072a5b4925267187a24469e49",
+    "url": "/index.html"
+  }
+];
