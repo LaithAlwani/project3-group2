86cfc2836a54ef43e6ea4b1e0afea61c8ee7fbaf
Magdi Tiea <magditiea@gmail.com>
2021-05-06 01:29:17 -0400

    removed unused css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,19 +9,9 @@ const Home = () => {
   return (
       <div>
        
-
+{/* title container */}
         <div id="orange">
         <h1 id="title">MY TEAM</h1>
-{/* 
-        <div className="home"> */}
-          {/* <Link to="/register" className="mr-4">
-            Sign Up
-          </Link>
-
-          <br></br>
-
-         <Link to="/login" className="mr-4"> Login </Link> */}
-        {/* </div> */}
         </div> 
 
 {/* button */}
@@ -46,46 +36,11 @@ const Home = () => {
   <div class="container">
     <div class="row">
       <div class="col-md-3 col-sm-6">
-        {/* <!--Column1--> */}
-        {/* <div class="footer-pad">
-          <h4>Heading 1</h4>
-          <ul class="list-unstyled">
-            <li><a href="#"></a></li>
-            <li><a href="#">Payment Center</a></li>
-            <li><a href="#">Contact Directory</a></li>
-            <li><a href="#">Forms</a></li>
-            <li><a href="#">News and Updates</a></li>
-            <li><a href="#">FAQs</a></li>
-          </ul>
-        </div> */}
       </div>
       <div class="col-md-3 col-sm-6">
-        {/* <!--Column1--> */}
-        {/* <div class="footer-pad">
-          <h4>Heading 2</h4>
-          <ul class="list-unstyled">
-            <li><a href="#">Website Tutorial</a></li>
-            <li><a href="#">Accessibility</a></li>
-            <li><a href="#">Disclaimer</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">FAQs</a></li>
-            <li><a href="#">Webmaster</a></li>
-          </ul>
-        </div> */}
       </div>
       <div class="col-md-3 col-sm-6">
-        {/* <!--Column1--> */}
-        {/* <div class="footer-pad">
-          <h4>Heading 3</h4>
-          <ul class="list-unstyled">
-            <li><a href="#">Parks and Recreation</a></li>
-            <li><a href="#">Public Works</a></li>
-            <li><a href="#">Police Department</a></li>
-            <li><a href="#">Fire</a></li>
-            <li><a href="#">Mayor and City Council</a></li>
-            <li><a href="#"></a></li>
-          </ul>
-        </div> */}
+
       </div>
     	<div class="col-md-3">
     		<h4>Creators</h4>
@@ -130,12 +85,3 @@ export default Home;
 
 
 
-
-
-
-
-
-
-
-
-{/* <div class="social"><a href="#"><i class="icon ion-social-github"></i></a><a href="#"><i class="icon ion-social-github"></i></a><a href="#"><i class="icon ion-social-github"></i></a></div> */}
