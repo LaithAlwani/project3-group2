31ec5bdd0f4ae2a7fb7b79fc951df9a0bdcdf331
Magdi Tiea <magditiea@gmail.com>
2021-05-07 00:53:23 -0400

    homepage structure touch up

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,64 +3,68 @@ import { Link } from "react-router-dom";
 import "../styles/Home.css";
 
 const Home = () => {
-
-
-
   return (
-      <div>
+    <div>
+      {/* title container */}
 
-
-{/* title container */}
-
-<div id="orange">
-  <div id="titlebox">
-        <h1 id="title">MY TEAM</h1>
+      <div id="orange">
+        <div id="titlebox">
+          <h1 id="title">MY TEAM</h1>
         </div>
+      </div>
 
-{/* button */}
-<div id="buttoncontainer">
- <Link to="/register" className="mr-4"><button className="button" type="button">
-            Sign Up   </button> </Link>
+      {/* button */}
+      <div id="buttoncontainer">
+        <Link to="/register" className="mr-4">
+          <button className="button" type="button">
+            Sign Up{" "}
+          </button>{" "}
+        </Link>
 
-<br></br>
+        <br></br>
 
-  <Link to="/login" className="mr-4"><button className="button">Login </button> </Link>         
-  </div>  
+        <Link to="/login" className="mr-4">
+          <button className="button">Login </button>{" "}
+        </Link>
+      </div>
 
-<div id="words">
-  <h3 id="keywords"> Chat</h3>   <h3 id="keywords" dangerouslySetInnerHTML={{__html:'Build'}}></h3> 
-  </div>
-
-
-
-
-
-{/* footer */}
-<div class="footer">
-
-<div class="footer">
-  <p class="footer-text-left">
-    <a href="index.html" class="menu">home</a>
-    <a href="https://css.sammy-codes.com/about.html" class="menu">about</a>
-    <a href="https://css.sammy-codes.com/credits.html" class="menu">credits</a>
-  </p>
-</div>
-<div class="footer-content-right">
-  <a href="https://github.com/digitalocean"><img src="images/github.jpeg" class="icon-style" alt="Github icon"/></a>
-  <a href="https://www.twitter.com/DigitalOcean"><img src="images/twitter.jpeg" class="icon-style" alt="Twitter icon"/></a>
-  <a href="https://www.twitter.com"><img src="images/email.jpeg" class="icon-style" alt="Emailicon"/></a>
-</div>
-</div>
- 
-  </div> 
-  </div>
-  
-  )
-}
-
+      {/* footer */}
+      <div class="footer">
+        <div class="footer">
+          <p class="footer-text-left">
+            <a href="index.html" class="menu">
+              home
+            </a>
+            <a href="https://css.sammy-codes.com/about.html" class="menu">
+              about
+            </a>
+            <a href="https://css.sammy-codes.com/credits.html" class="menu">
+              credits
+            </a>
+          </p>
+        </div>
+        <div class="footer-content-right">
+          <a href="https://github.com/digitalocean">
+            <img
+              src="images/github.jpeg"
+              class="icon-style"
+              alt="Github icon"
+            />
+          </a>
+          <a href="https://www.twitter.com/DigitalOcean">
+            <img
+              src="images/twitter.jpeg"
+              class="icon-style"
+              alt="Twitter icon"
+            />
+          </a>
+          <a href="https://www.twitter.com">
+            <img src="images/email.jpeg" class="icon-style" alt="Emailicon" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
-
-
-
-
