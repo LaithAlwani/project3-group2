7a353d4c77b853b36532e0e257760e4131a988b1
Laith Alwani <laithalwani@gmail.com>
2021-05-10 19:29:32 -0400

    models folder added

---
+++ models/team.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const TeamSchema = new mongoose.Schema({
+  teamName: {
+    type: String,
+    required: [true, "Please provide a Team name"]
+  },
+  teamImage:{
+    type:String,
+    default:"team-image.png"
+  },
+  sport:{
+      type:String,
+      required: [true, "please enter the sport that your team plays"]  
+  },
+  players: [
+    {
+      player: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+
+      isAdmin: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  ],
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      }
+  ],
+  teamCreated: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Team = mongoose.model("Team", TeamSchema);
+
+module.exports = Team;
